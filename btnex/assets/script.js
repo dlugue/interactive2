5b721a55186092b6e7f01e37a3f9e981ea0a8b03
Dionne Lugue <dlugue@cca.edu>
2017-09-11 05:43:50 -0700

    Updated with 50 Buttons

--- btnex/assets/script.js
+++ btnex/assets/script.js
@@ -1,12 +1,16 @@
 $(document).ready(function(){
 	
-	$('#button-3').click(function() {
-		$(this).animate({
-			opacity: 0.4,
-			marginLeft: '0.6in',
-			fontSize: '24px',
-			borderWidth: '10px'
-		}, 1000 );
-	});
+
+	jQuery(function($) {
+    $('#button-48').mouseover(function() {
+        var dWidth = $(document).width() - 100, // 100 = image width
+            dHeight = $(document).height() - 100, // 100 = image height
+            nextX = Math.floor(Math.random() * dWidth),
+            nextY = Math.floor(Math.random() * dHeight);
+        $(this).animate({ left: nextX + 'px', top: nextY + 'px' });
+    });
+});
+
+
 
 });
